45941170583b59453d3b36108c8e92a07c8b1fee
alinutzaB <alinutza.ms@gmail.com>
2019-11-14 22:53:43 +0200

    Updates

---
+++ precache-manifest.413ba46c306adcb197d67c777fe35877.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9646049c86844d135777f8edc166a4f",
+    "url": "/crudReactApp/index.html"
+  },
+  {
+    "revision": "df41dec257dda9756935",
+    "url": "/crudReactApp/static/css/main.bf286928.chunk.css"
+  },
+  {
+    "revision": "5cc110df5496c3dfcb98",
+    "url": "/crudReactApp/static/js/2.51e9f512.chunk.js"
+  },
+  {
+    "revision": "df41dec257dda9756935",
+    "url": "/crudReactApp/static/js/main.55b9c4a2.chunk.js"
+  },
+  {
+    "revision": "e9d309a57ef70973a643",
+    "url": "/crudReactApp/static/js/runtime-main.6fa029ad.js"
+  }
+]);
